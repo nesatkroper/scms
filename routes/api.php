a6fe70f80b338b9da99f14b946c538104ceb7e4f
phanunsuon <phanunsuon@gmail.com>
2025-06-25 04:27:15 +0000

    modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-use App\Http\Controllers\{
+use App\Http\Controllers\Api\{
     AuthController,
     AttendanceController,
     BookController,
