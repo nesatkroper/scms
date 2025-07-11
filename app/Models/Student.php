e30c9442f378fc142901efb4a236e453907b8b6a
megalobob <phanunsuon@gmail.com>
2025-07-11 19:42:19 +0700

    modified:   app/Models/Student.php

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
   use HasFactory;
-  protected $fillable = ['user_id', 'admission_date', 'section_id'];
+  protected $fillable = ['user_id', 'admission_date', 'section_id', ''];
 
   public function user()
   {
