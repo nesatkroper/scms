42a8a51c20147256da97478549899cf53890b555
LAV-TIT <lavtit.nssp2021@gmail.com>
2025-07-19 09:43:16 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -73,96 +73,28 @@ R::prefix('/admin')
 
         ]);
 
+        $bulkRoutes = [
+            'expenses' => ExpenseController::class,
+            'sections' => SectionController::class,
+            'bookissues' => BookIssueController::class,
+            'books' => BookController::class,
+            'students' => StudentController::class,
+            'guardians' => GuardianController::class,
+            'departments' => DepartmentController::class,
+            'gradelevels' => GradeLevelController::class,
+            'subjects' => SubjectController::class,
+            'teachers' => TeacherController::class,
+        ];
 
-        R::prefix('/expenses')
-            ->as('expenses.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [ExpenseController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [ExpenseController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [ExpenseController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/sections')
-            ->as('sections.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [SectionController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [SectionController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [SectionController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/bookissues')
-            ->as('bookissues.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [BookIssueController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/get-bulk-data', [BookIssueController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [BookIssueController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/books')
-            ->as('books.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulkDelete', [BookController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/getBulkData', [BookController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulkUpdate', [BookController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/students')
-            ->as('students.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [StudentController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [StudentController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [StudentController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/guardians')
-            ->as('guardians.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [GuardianController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [GuardianController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [GuardianController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/departments')
-            ->as('departments.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [DepartmentController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [DepartmentController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [DepartmentController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/gradelevels')
-            ->as('gradelevels.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [GradeLevelController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [GradeLevelController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [GradeLevelController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/subjects')
-            ->as('subjects.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [SubjectController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [SubjectController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [SubjectController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
-
-        R::prefix('/teachers')
-            ->as('teachers.')
-            ->middleware('auth')
-            ->group(function () {
-                R::post('/bulk-delete', [TeacherController::class, 'bulkDelete'])->name('bulkDelete');
-                R::post('/bulk-data', [TeacherController::class, 'getBulkData'])->name('getBulkData');
-                R::post('/bulk-update', [TeacherController::class, 'bulkUpdate'])->name('bulkUpdate');
-            });
+        foreach ($bulkRoutes as $prefix => $controller) {
+            R::prefix($prefix)
+                ->as($prefix . '.')
+                ->group(function () use ($controller) {
+                    R::post('/bulk-delete', [$controller, 'bulkDelete'])->name('bulkDelete');
+                    R::post('/bulk-data', [$controller, 'getBulkData'])->name('getBulkData');
+                    R::post('/bulk-update', [$controller, 'bulkUpdate'])->name('bulkUpdate');
+                });
+        }
 
         R::prefix('/students/{student}/guardians')
             ->as('student_guardians.')
