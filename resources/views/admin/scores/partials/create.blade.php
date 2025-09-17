835f8b054390e8622dbcda2a0562ec42558ad66d
LAV-TIT <lavtit.nssp2021@gmail.com>
2025-09-17 15:39:59 +0700

    modified:   composer.lock 	modified:   resources/views/admin/scores/partials/create.blade.php

--- resources/views/admin/scores/partials/create.blade.php
+++ resources/views/admin/scores/partials/create.blade.php
@@ -1,5 +1,5 @@
 <!-- Create Modal -->
-<x-modal.modal id="Modalcreate" title="Create New Teacher" class="rounded-sm w-full max-w-full h-full max-h-full"
+<x-modal.modal id="Modalcreate" title="Create New Score" class="rounded-sm w-full max-w-full h-full max-h-full"
     svgPath="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z">
     <!-- Form Content -->
     <form action="{{ route('admin.scores.store') }}" method="POST" class="p-4">
