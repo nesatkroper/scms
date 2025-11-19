0bb072eaed8c4c5297fb72a0e6edf4a5cbd11580
TIT <you@example.com>
2025-11-19 15:19:44 +0700

    Update create.blade.php

--- resources/views/admin/students/partials/create.blade.php
+++ resources/views/admin/students/partials/create.blade.php
@@ -3,7 +3,7 @@
     
     <form action="{{ route('admin.students.store') }}" method="POST" class="pt-4 needs-validation" novalidate enctype="multipart/form-data">
         @csrf
-        <div class="h-[65vh] md:h-[70vh] overflow-y-auto px-4">
+        <div class="h-[52vh] overflow-y-auto px-4">
             <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mb-2">
                 <x-upload-file />
                 <div class="mb-2">
@@ -15,7 +15,7 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-2 mb-2">
                 <x-fields.input type="email" label="Email" name="email" placeholder="Enter email address" :required="true" />
                 <x-fields.input type="tel" label="Phone" placeholder="Enter phone number" name="phone" :required="true" />
-                <x-fields.select label="Grade Level" name="grade_level_id" :options="$gradeLevels->pluck('name', 'id')->toArray()" :required="true" />
+                {{-- <x-fields.select label="Grade Level" name="grade_level_id" :options="$gradeLevels->pluck('name', 'id')->toArray()" :required="true" /> --}}
                 <x-fields.input label="Blood Group" name="blood_group" placeholder="Enter blood group" max="10" maxlength="10"/>
                 <x-fields.input label="Nationality" name="nationality" placeholder="Enter nationality" />
                 <x-fields.input label="Religion" name="religion" placeholder="Enter religion" />
