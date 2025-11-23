391e18d254e53445444dbe7257465d98a4c143b1
TIT <you@example.com>
2025-11-23 22:12:59 +0700

    modified:   resources/views/admin/students/index.blade.php 	modified:   resources/views/admin/teachers/index.blade.php

--- resources/views/admin/teachers/index.blade.php
+++ resources/views/admin/teachers/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('title', 'Teachers')
 @section('content')
-    <x-page.index btn-text="Create New Teacher" :showReset="true" :showViewToggle="true" title="Teacher"
+    <x-page.index :btnCreate="true" btn-text="Create New Teacher" :showReset="true" :showViewToggle="true" title="Teacher"
         iconSvgPath="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z">
         <div id="TableContainer" class="table-respone overflow-x-auto h-[60vh]">
             @include('admin.teachers.partials.table', ['teachers' => $teachers])
