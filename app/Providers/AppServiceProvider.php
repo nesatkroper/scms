4a81edf617c8bb27783a4d1b1f084f404aa49c3c
Dev. Nun <phanunsuon@gmail.com>
2025-12-17 16:43:26 +0700

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\PersonalAccessToken;
 use Laravel\Sanctum\Sanctum;
@@ -11,8 +12,8 @@ class AppServiceProvider extends ServiceProvider
   public function register(): void {}
 
   public function boot(): void
-
   {
     Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+    URL::forceHttps(); //for vercel
   }
 }
